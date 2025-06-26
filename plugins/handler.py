71c8171a14a154e7fcc49730fc86e5c3502d69d8
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-26 16:21:16 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -10,93 +10,100 @@ from bot import Bot
 # Handler untuk pesan privat
 @Bot.on_message()
 async def on_message(client: Client, msg: Message):
-    if msg.chat.type == enums.ChatType.PRIVATE:
-        if msg.from_user is None:
-            return
+    if msg.chat.type != enums.ChatType.PRIVATE or msg.from_user is None:
+        return
 
-        uid = msg.from_user.id
-        helper = Helper(client, msg)
-        database = Database(uid)
+    uid = msg.from_user.id
+    helper = Helper(client, msg)
+    database = Database(uid)
 
-        # Cek langganan channel
-        if not await helper.cek_langganan_channel(uid):
-            return await helper.pesan_langganan()
+    # Cek langganan channel
+    if not await helper.cek_langganan_channel(uid):
+        return await helper.pesan_langganan()
 
-        if not await database.cek_user_didatabase():
-            await helper.daftar_pelanggan()
-            await helper.send_to_channel_log(type="log_daftar")
+    # Cek dan daftar user jika belum ada di database
+    if not await database.cek_user_didatabase():
+        await database.tambah_databot()
+        await helper.send_to_channel_log(type="log_daftar")
 
-        if not database.get_data_bot(client.id_bot).bot_status:
-            status = ['member', 'banned']
-            member = database.get_data_pelanggan()
-            if member.status in status:
-                return await client.send_message(uid, "<i>Saat ini bot sedang dinonaktifkan</i>", enums.ParseMode.HTML)
+    member = database.get_data_pelanggan()
 
-        command = msg.text or msg.caption
+    # Cek status bot (dari data pengguna, bukan get_data_bot)
+    if not member.json.get("bot_status", True):
+        if member.status in ['member', 'banned']:
+            return await client.send_message(uid, "<i>Saat ini bot sedang dinonaktifkan</i>", enums.ParseMode.HTML)
 
-        if command:
-            if command == '/start':
-                return await start_handler(client, msg)
-            elif command == '/help':
-                return await help_handler(client, msg)
-            elif command == '/status':
-                return await status_handler(client, msg)
-            elif command == '/list_admin':
-                return await list_admin_handler(helper, client.id_bot)
-            elif command == '/list_ban':
-                return await list_ban_handler(helper, client.id_bot)
-            elif command == '/stats':
-                if uid == config.id_admin:
-                    return await statistik_handler(helper, client.id_bot)
-            elif command == '/broadcast':
-                if uid == config.id_admin:
-                    return await broadcast_handler(client, msg)
-            elif command in ['/settings', '/setting']:
-                member = database.get_data_pelanggan()
-                if member.status in ['admin', 'owner']:
-                    return await setting_handler(client, msg)
-            elif re.search(r"^[\/]{1}tf_coin", command):
-                return await transfer_coin_handler(client, msg)
-            elif re.search(r"^[\/]{1}bot", command):
-                if uid == config.id_admin:
-                    return await bot_handler(client, msg)
-            elif re.search(r"^[\/]{1}admin", command):
-                if uid == config.id_admin:
-                    return await tambah_admin_handler(client, msg)
-            elif re.search(r"^[\/]{1}unadmin", command):
-                if uid == config.id_admin:
-                    return await hapus_admin_handler(client, msg)
-            elif re.search(r"^[\/]{1}ban", command):
-                member = database.get_data_pelanggan()
-                if member.status in ['admin', 'owner']:
-                    return await ban_handler(client, msg)
-            elif re.search(r"^[\/]{1}unban", command):
-                member = database.get_data_pelanggan()
-                if member.status in ['admin', 'owner']:
-                    return await unban_handler(client, msg)
+    command = msg.text or msg.caption
+    if not command:
+        return
 
-            # Deteksi hashtag
-            x = re.search(fr"(?:^|\s)({config.hastag})", command.lower())
-            if x:
-                key = x.group(1)
-                hastag = config.hastag.split('|')
-                member = database.get_data_pelanggan()
+    # ======== Command Handler =========
+    if command == '/start':
+        return await start_handler(client, msg)
+    elif command == '/help':
+        return await help_handler(client, msg)
+    elif command == '/status':
+        return await status_handler(client, msg)
+    elif command == '/list_admin':
+        return await list_admin_handler(helper, client.id_bot)
+    elif command == '/list_ban':
+        return await list_ban_handler(helper, client.id_bot)
+    elif command == '/stats':
+        if uid == config.id_admin:
+            return await statistik_handler(helper, client.id_bot)
+    elif command == '/broadcast':
+        if uid == config.id_admin:
+            return await broadcast_handler(client, msg)
+    elif command in ['/settings', '/setting']:
+        if member.status in ['admin', 'owner']:
+            return await setting_handler(client, msg)
+    elif re.match(r"^/tf_coin", command):
+        return await transfer_coin_handler(client, msg)
+    elif re.match(r"^/bot", command):
+        if uid == config.id_admin:
+            return await bot_handler(client, msg)
+    elif re.match(r"^/admin", command):
+        if uid == config.id_admin:
+            return await tambah_admin_handler(client, msg)
+    elif re.match(r"^/unadmin", command):
+        if uid == config.id_admin:
+            return await hapus_admin_handler(client, msg)
+    elif re.match(r"^/ban", command):
+        if member.status in ['admin', 'owner']:
+            return await ban_handler(client, msg)
+    elif re.match(r"^/unban", command):
+        if member.status in ['admin', 'owner']:
+            return await unban_handler(client, msg)
 
-                if member.status == 'banned':
-                    return await msg.reply(f'Kamu telah <b>di banned</b>\n\n<u>Alasan:</u> {database.get_data_bot(client.id_bot).ban[str(uid)]}\nsilahkan kontak admin @xvilance untuk unbanned', True, enums.ParseMode.HTML)
+    # ======== Deteksi Hashtag untuk Menfess =========
+    x = re.search(fr"(?:^|\s)({config.hastag})", command.lower())
+    if x:
+        key = x.group(1)
+        hastag_list = config.hastag.split('|')
 
-                if key in hastag:
-                    if key == command.lower() or len(command.split(' ')) < 3:
-                        return await msg.reply('\ud83d\ude45\u200d\u2640\ufe0f  post gagal terkirim, <b>mengirim pesan wajib lebih dari 3 kata.</b>', True, enums.ParseMode.HTML)
-                    else:
-                        return await send_menfess_handler(client, msg)
-                else:
-                    await gagal_kirim_handler(client, msg)
+        if member.status == 'banned':
+            alasan = member.json.get("ban", {}).get(str(uid), "tidak diketahui")
+            return await msg.reply(
+                f'Kamu telah <b>di banned</b>\n\n<u>Alasan:</u> {alasan}\nSilakan kontak admin @xvilance untuk unbanned',
+                parse_mode=enums.ParseMode.HTML
+            )
+
+        if key in hastag_list:
+            if key == command.lower() or len(command.split(' ')) < 3:
+                return await msg.reply(
+                    '🙅‍♀️ Post gagal terkirim, <b>mengirim pesan wajib lebih dari 3 kata.</b>',
+                    parse_mode=enums.ParseMode.HTML
+                )
             else:
-                await gagal_kirim_handler(client, msg)
-                
+                return await send_menfess_handler(client, msg)
+
+    # Jika tidak sesuai
+    await gagal_kirim_handler(client, msg)
+
+
 async def gagal_kirim_handler(client, msg):
     await msg.reply("🚫 Kamu tidak diizinkan mengakses fitur ini. Harap hubungi admin jika kamu merasa ini kesalahan.")
+
 
 # Handler callback tombol
 @Bot.on_callback_query()
@@ -114,7 +121,7 @@ async def on_callback_query(client: Client, query: CallbackQuery):
             if query.message.chat.id == config.id_admin:
                 await status_handler_inline(client, query)
             else:
-                await query.answer('Ditolak, kamu tidak ada akses', True)
+                await query.answer('Ditolak, kamu tidak ada akses', show_alert=True)
         elif data == 'ya_confirm':
             await broadcast_ya(client, query)
         elif data == 'tidak_confirm':
@@ -132,6 +139,7 @@ async def on_callback_query(client: Client, query: CallbackQuery):
                 await query.answer("⚠️ Gagal cek. Mungkin kamu belum join?", show_alert=True)
     except:
         pass
+
 
 # Cek otomatis keanggotaan user di channel 1 saat kirim pesan ke channel 2
 @Bot.on_message(filters.chat(config.channel_2))
