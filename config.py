254cdddf06f41f5809a204fc332544e89113c999
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-26 14:58:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ channel_log = int(os.getenv("CHANNEL_LOG", "-1001634100538"))
 # =========================================================== #
 
 id_admin = int(os.getenv("ID_ADMIN", "732448606"))
-admin = int(os.getenv("ADMIN", "732448606"))
+admin = [732448606]  # Ganti 7316811520 dengan ID Telegram kamu (sebagai admin)
 # =========================================================== #
 
 batas_kirim = int(os.getenv("BATAS_KIRIM", "2"))
