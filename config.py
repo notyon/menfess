713f2eaa1a850f38e387253c1e403b37889494e6
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-26 14:16:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ bot_token = os.getenv("BOT_TOKEN", "")
 # =========================================================== #
 
 db_url = os.getenv("DB_URL", "mongodb+srv://ucik:ucik@cluster0.0l3r8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db_name = os.getenv("DB_NAME", "leo1") #bisa diganti sesuai kebutuhan
+db_name = os.getenv("DB_NAME", "menfessztest") #bisa diganti sesuai kebutuhan
 # =========================================================== #
 
-channel_1 = int(os.getenv("CHANNEL_1", "-1001991260453"))
-channel_2 = int(os.getenv("CHANNEL_2", "-1001841428029")) #untuk group comentar user
-channel_log = int(os.getenv("CHANNEL_LOG", "-1002068950974"))
+channel_1 = int(os.getenv("CHANNEL_1", "-1001809070827"))
+channel_2 = int(os.getenv("CHANNEL_2", "-1002393756718")) #untuk group comentar user
+channel_log = int(os.getenv("CHANNEL_LOG", "-1001634100538"))
 # =========================================================== #
 
 id_admin = int(os.getenv("ID_ADMIN", "732448606"))
@@ -26,7 +26,7 @@ batas_kirim = int(os.getenv("BATAS_KIRIM", "2"))
 biaya_kirim = int(os.getenv("BIAYA_KIRIM", "100"))
 # =========================================================== #
 
-hastag = os.getenv("HASTAG", "#111 #112 #boy #girl #spill #ask #pap #story").replace(" ", "|").lower()
+hastag = os.getenv("HASTAG", "#111 #112 #test #boy #girl #spill #ask #pap #story").replace(" ", "|").lower()
 # =========================================================== #
 
 pic_boy = os.getenv("PIC_BOY", "https://telegra.ph/file/c67bd36023648dc777bd9.jpg")
