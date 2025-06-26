495f6610e6cd46904557b8ad957b39b46e0ea759
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-26 15:18:19 +0700

    Update database.py

--- plugins/database/database.py
+++ plugins/database/database.py
@@ -46,25 +46,6 @@ class Database():
     def __init__(self, user_id: int):
         self.user_id = user_id
 
-    async def tambah_databot(self):
-        if mycol.find_one({"_id": self.user_id}):
-            return
-
-        data = {
-            "_id": self.user_id,
-            "status": "non-member",
-            "menfess": 0,
-            "bot_status": True,
-            "ban": {},
-            "admin": [],
-            "kirimchannel": {
-                "photo": True,
-                "video": False,
-                "voice": False
-            }
-        }
-        await self.tambah_pelanggan(data)
-
     async def cek_user_didatabase(self):
         found = mycol.find_one({'_id': self.user_id})
         if found:
